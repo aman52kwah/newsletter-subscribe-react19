6373ffcc85ae04c9e20f61e45f86a78bac9749d3
aman52kwah <amankwaamichael52@gmail.com>
2025-02-25 01:31:48 +0100

    useActionState used for the new codes

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # React + Vite
+ Here in thid first codes comment :
+ the form inputs are controlled and their values are saved in state
+loading and error states are manually handled
+
+Now here is the same form with React 19
+
+form uses useActionState hook to delegate handling of the loading and error states
+form is submitted with the action attribute instead of onSubmit
+form no longer needs controlled inputs, as the values are correctly populated (and cleared!) when we use the action attribute to submit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/components/NewsletterSubscribe.jsx
+++ src/components/NewsletterSubscribe.jsx
@@ -1,81 +1,173 @@
-import {useState} from "react";
-import "./newsletter.css";
+// import {useState} from "react";
+// import "./newsletter.css";
 
-const fakeSendEmail = async () =>{
-return new Promise((resolve)=> setTimeout(resolve,1000));
-};
+// const fakeSendEmail = async () =>{
+// return new Promise((resolve)=> setTimeout(resolve,1000));
+// };
 
-const NewsletterSubscribe = () => {
-  const [name,setName]=useState("");
-  const [email,setEmail]=useState("");
-  const [result,setResult]=useState(null);
-  const [isPending,setIsPending]=useState(false);
+// const NewsletterSubscribe = () => {
+//   const [name,setName]=useState("");
+//   const [email,setEmail]=useState("");
+//   const [result,setResult]=useState(null);
+//   const [isPending,setIsPending]=useState(false);
 
 
  
    
-  const handleSubmit = (e) => {
-    e.preventDefault();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
 
-  if (!name || !email) {
-    setResult({
-    type:"error",
-    message:"please fill in your fullname and email address.",
-    });
-    return;
-  }
-    setIsPending(true);
-    fakeSendEmail().then(()=>{
-      setResult({
-        type:"success",
-        message:"You have successfully subscribed to my newsletter"
-      });
-      setName("");
-      setEmail("");
-      setIsPending(false);
-    });
-  };
+//   if (!name || !email) {
+//     setResult({
+//     type:"error",
+//     message:"please fill in your fullname and email address.",
+//     });
+//     return;
+//   }
+//     setIsPending(true);
+//     fakeSendEmail().then(()=>{
+//       setResult({
+//         type:"success",
+//         message:"You have successfully subscribed to my newsletter"
+//       });
+//       setName("");
+//       setEmail("");
+//       setIsPending(false);
+//     });
+//   };
 
-      return (
-  <>
-  {result && <p className={`message ${result.type}`}>{result.message}</p>}
-  {isPending && <p className="message loading">Loading..</p>}
+//       return (
+//   <>
+//   {result && <p className={`message ${result.type}`}>{result.message}</p>}
+//   {isPending && <p className="message loading">Loading..</p>}
 
-       {/* form inputs */}
-       <form onSubmit={handleSubmit}>
-    <h3>Join the newsletter</h3>
-    <div>
-      <label htmlFor="name">Name</label>
-      <input 
-      type="text"
-      name="name"
-      id="name"
-      value={name}
-      onChange={(e) => setName(e.target.value)}
-      />
-    </div>
+//        {/* form inputs */}
+//        <form onSubmit={handleSubmit}>
+//     <h3>Join the newsletter</h3>
+//     <div>
+//       <label htmlFor="name">Name</label>
+//       <input 
+//       type="text"
+//       name="name"
+//       id="name"
+//       value={name}
+//       onChange={(e) => setName(e.target.value)}
+//       />
+//     </div>
 
-    <div>
-      <label htmlFor="email">Email</label>
+//     <div>
+//       <label htmlFor="email">Email</label>
       
-      <input 
-      type="email"
-      name="email"
-      id="email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-  />
-    </div>
+//       <input 
+//       type="email"
+//       name="email"
+//       id="email"
+//       value={email}
+//       onChange={(e) => setEmail(e.target.value)}
+//   />
+//     </div>
 
-    <div>
-      <button type="submit">Subscribe</button>
-    </div>
+//     <div>
+//       <button type="submit">Subscribe</button>
+//     </div>
 
-  </form>
+//   </form>
   
   
-  </>
-)
+//   </>
+// )
+// };
+import { useActionState } from "react";
+import "./newsletter.css";
+
+
+const fakeSendEmail =async () => {
+  return new Promise((resolve) => setTimeout(resolve,1000));
 };
+
+  const NewsletterSubscribe = () => {
+    const [result,submitAction,isPending] = useActionState(
+      async (previousState, FormData) => {
+        const email =FormData.get("email");
+        const name =FormData.get("name");
+
+        // checking errors with if condition
+        if (!name || !email){
+          return {
+            type:"error",
+            message:`Please fill in your name and email.`,
+          };
+        }
+
+        await fakeSendEmail();
+        
+        
+        return{
+          type:"success",
+          message:`You have successfully subscribed!`,
+        };
+      },
+      null
+    );
+
+    return(
+      <>
+      {result && <p className={`message ${result.type}`}>{result.message}</p>}
+      {isPending && <p className="message loading">Loading...</p>}
+
+        <form action={submitAction}>
+        <h3>Join the newsletter</h3>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" 
+          id="name" 
+          name="name" />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email"
+          id="email" 
+          name="email" />
+        </div>
+        <div>
+          <button type="submit">Subscribe</button>
+        </div>
+
+        </form>
+
+      </>
+    )
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default NewsletterSubscribe;
